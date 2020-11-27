b0b2deb1126c1f4532c10eb597da127aaad1fae4
Artem <rozen19942810@gmail.com>
2020-11-27 12:22:10 +0200

    Updates

---
+++ precache-manifest.877f16fb9023979c72572ce8be97616f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "555b05b3bd14a87537ce27056b2f4027",
+    "url": "/TodoList/index.html"
+  },
+  {
+    "revision": "4ebf1bd5dabe2ab051e5",
+    "url": "/TodoList/static/css/main.674b6137.chunk.css"
+  },
+  {
+    "revision": "eedc512af3c91e6cc62b",
+    "url": "/TodoList/static/js/2.317bd65b.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/TodoList/static/js/2.317bd65b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ebf1bd5dabe2ab051e5",
+    "url": "/TodoList/static/js/main.4c26a169.chunk.js"
+  },
+  {
+    "revision": "6f4002f704c746794b97",
+    "url": "/TodoList/static/js/runtime-main.b918c17e.js"
+  }
+]);
